664cbaa110572ff61727b208cda45b84429cf0a4
Jaewon Chung <jaewonchung97@gmail.com>
2023-12-05 11:01:44 +0900

    Spring Initialized

---
+++ src/main/java/kr/ac/gachon/oplanner/OplannerApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.gachon.oplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OplannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OplannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/ac/gachon/oplanner/OplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.gachon.oplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OplannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
